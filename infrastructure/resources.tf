0a583e507083b43f1f0f8baa66dbbbb253c9a35f
Stephen Hopkins <steve417@gmail.com>
2020-11-21 18:11:24 +1000

    fix: blob public access, upload script

--- infrastructure/resources.tf
+++ infrastructure/resources.tf
@@ -83,10 +83,11 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                = local.storagename
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  tags                = local.tags
+  name                     = local.storagename
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  tags                     = local.tags
+  allow_blob_public_access = true
 
   account_kind             = "StorageV2"
   account_tier             = "Standard"
